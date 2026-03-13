df5fcce9fcf3fb28577b87bb2d700024053bd7aa
greg-tribebuilder <greg@tribebuilder.dev>
2026-03-13 03:54:19 -0700

    fix: Correct Listing and User approval Id display conditions
    Listing and User were inadvertently sharing a display condition

--- nodes/Sharetribe/v1/actions/user/approve/description.ts
+++ nodes/Sharetribe/v1/actions/user/approve/description.ts
@@ -10,14 +10,14 @@ import {
 } from '../../../helpers/Sharetribe';
 
 export const approveUserDescription: UserProperties = [
-	makeResourceIdField(UI_RESOURCES.USER, UI_OPERATIONS.APPROVE, {
+	makeResourceIdField(UI_RESOURCES.USER, UI_OPERATIONS.APPROVE_USER, {
 		displayName: 'User ID',
 		name: 'userId',
 		description:
 			"Command for approving a user that is currently in pendingApproval state. The user's state is set to active.",
 	}),
-	makeSimplifyField(UI_RESOURCES.USER, UI_OPERATIONS.APPROVE),
-	makeFieldsToReturnField(UI_RESOURCES.USER, UI_OPERATIONS.APPROVE, {
+	makeSimplifyField(UI_RESOURCES.USER, UI_OPERATIONS.APPROVE_USER),
+	makeFieldsToReturnField(UI_RESOURCES.USER, UI_OPERATIONS.APPROVE_USER, {
 		name: 'userFields',
 		defaults: [...RESOURCE_DEFAULTS.user],
 		options: USER_RELATIONSHIP_OPTIONS,
